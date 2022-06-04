0e5be0125416df43ebf648782eadb53a063ee6e0
lucia <luciaahab@gmail.com>
2022-06-04 22:12:06 +0200

    duro, duro

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,21 +1,85 @@
+'''
+nota
+Kike: !!! No me sale y NO sé por qué. No lo sé. Creo que voy a borrar todo y a hacer un archivo nuevo entregable. **cries in python
+
+'''
+
 
 # Soldier
 
 
-class Soldier:
-    pass
+class Soldier: #Aquí se define el objeto tipo soldado. características comunes
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+#Abajo son cosas que los soldado pueden hacer. Acciones
+#attack
+    def attack(self):
+        return self.strength
+#damage
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health = self.health - self.damage
+         
+    
 
 # Viking
 
+'''
+class Viking(Soldier): #El vikingo es un tipo de soldado, comparte. Hija
+    def __init__(self, name, health, strength): #constructor
+        Soldier().__init__(health, strenght)
+        self.name = name
+            
+            
+    def receiveDamage(self, damage): #metodo
 
-class Viking:
-    pass
+        self.health -= self.health -damage
+            
+        if self.health > 0: 
+            return f"{self.name}, 'has received', {self.damage}, 'points of damage'"
+            
+        else: 
+            return f"{self.name}, 'has has died in combat'"
+        
+    def battleCry(self): #metodo
+            return 'Odin Owns You All!'
+#El viernes me daba mal por la puta IDENTACIÓN. No desesperarr
+'''
+class Viking(Soldier):
+    name = ''
+    def name(self):
+        self.name= "Petrus"
+        
+    def receiveDamage(self, damage): #metodo
+        self.health -= self.health -damage
+            
+        if self.health > 0: 
+            return f"{self.name}, 'has received', {self.damage}, 'points of damage'"
+            
+        else: 
+            return f"{self.name}, 'has has died in combat'"
+        
+    def battleCry(self): #metodo
+            return 'Odin Owns You All!'
+
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health,strenth):
+        Soldier().__init(health,strenght)
+        
+    def receiveDamage(self,damage):
+        self.damage=damage
+        self.health=self.health - damage
+        
+        if self.health > 0:
+            return'A saxon has received', damage, 'points of damage'
+        else:
+            return 'A saxon has died in combat'
 
 # War
 
