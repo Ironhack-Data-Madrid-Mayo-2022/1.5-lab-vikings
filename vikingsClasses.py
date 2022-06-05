06c64ea6cab9e698c0515245241fad725f8f9401
Mau Guerrero <mgc_181@hotmail.com>
2022-06-05 12:58:15 +0200

    Sin Guerra

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,98 @@
 
 
 class Soldier:
-    pass
-
+    
+    def __init__(self,health, strength): # siempre que se inicie una "class" se debe iniciar con __init__
+        self.health = health
+        self.strength = strength
+       
+        
+    def attack (self):
+        return self.strength
+    
+    def receiveDamage(self,damage):
+        self.damage = damage
+        self.health -= self.damage
+    
 # Viking
 
 
 class Viking:
-    pass
+    
+    def __init__(self,name,health,strength):
+        
+        Soldier.__init__(self, health, strength)
+        self.name=name
+    
+    def attack (self):
+        return self.strength
+    
+    def receiveDamage(self,damage):
+        self.damage = damage
+        self.health -= self.damage
+        
+        if self.health>0: return f"{self.name} has received {self.damage} points of damage"
+                                      
+        else: return f"{self.name} has died in act of combat"
+    
+    def battleCry(self):
+        return 'Odin Owns You All!'
+        
+    
 
-# Saxon
-
+#Saxon
 
 class Saxon:
-    pass
+    
+    def __init__(self,health,strength):
+        
+        Soldier.__init__(self, health, strength)
+    
+    def attack (self):
+        return self.strength
+    
+    def receiveDamage(self,damage):
+        self.damage = damage
+        self.health -= self.damage
+        
+        if self.health>0: return f"A Saxon has received {self.damage} points of damage"
+                                       
+        else: return 'A Saxon has died in combat'   
 
 # War
 
 
 class War:
-    pass
+    
+    def __init__(self):
+         
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+    def addViking(self, viking):
+           self.vikingArmy.append(viking)
+    
+    
+    def addSaxon(self, saxon):
+           self.saxonArmy.append(saxon)
+  
+    
+    
+    def vikingAttack(self):
+        
+           if receiveDamage == strength
+    
+    
+    
+    
+    def saxonAttack(self):
+
+        
+        
+        
+    def showStatus(self):
+          
+        
+        
+        
+    
