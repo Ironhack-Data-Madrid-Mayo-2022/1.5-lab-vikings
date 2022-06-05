732ba27e8382ca347a9619e08723cc852da58952
Jorge Albors <jorgealbors@hotmail.com>
2022-06-05 15:47:22 +0200

    Sol 1

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,19 +3,56 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        
+        self.health = health
+        self.strength = strength
+        
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.damage = damage
+            
+        self.health -= self.damage
+            
+        
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        Soldier.__init__(self, health, strength)
+        
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health -= self.damage
+        if self.health > 0:
+            return self.name,"has received", self.damage, "points of damage"
+        else:
+            return self.name,"has died in act of combat"
+    def battleCry(self):
+        return "Odin Owns You All!"
+    
 
 # Saxon
 
-
 class Saxon:
-    pass
+    
+    def __init__(self, health, strength):
+        Soldier.__init__(self, health, strength)
+        
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health -= self.damage
+        if self.health > 0:
+            return "A Saxon has received", self.damage, "points of damage"
+        else:
+            return "A Saxon has died in combat"
+        
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # War
 
