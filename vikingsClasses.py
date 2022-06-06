831ad8c01bc16c87be2692384c00cdfefb86078e
iaguerri <ireneaguerri@gmail.com>
2022-06-06 08:36:20 +0200

    WAR not ok. Error List differ. Lo vemos luego porfi

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -58,15 +58,36 @@ class War:
         self.saxonArmy = saxonArmy
     
     
-    def addViking(self, vikingArmy):
-        for self in self.vikingArmy:
-            self.vikingArmy.append(self)
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
             
         
-    def addSaxon(self, saxonArmy):
-        for i in self.saxonArmy:
-            self.saxonArmy.append(i)
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
             
-#     def vikingAttack():
-#     def saxonAttack():
-#     def showStatus():
+    def vikingAttack():
+        viking = self.vikingArmy[0]
+        saxon = self.saxonArmy[0]
+        vikingBattle = saxon.receiveDamage(viking.strength)
+        if saxon.health <= 0: 
+            self.saxonArmy.pop(0)
+        return vikingBattle
+    
+    
+    def vikingAttack():
+        viking = self.vikingArmy[0]
+        saxon = self.saxonArmy[0]
+        saxonBattle = viking.receiveDamage(saxon.strength)
+        if viking.health <= 0: 
+            self.vikingArmy.pop(0)
+        return saxonBattle
+        
+    def showStatus():
+        if len(self.saxonArmy) <= 0:
+            return "Vikings have won the war of the century"
+        elif len(self.vikingArmy) <= 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.vikingArmy)>0 and len(self.vikingArmy)>0:
+            return "Vikings and Saxons are still in the thick of battle."
+            
+            
